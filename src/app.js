97b65a2d2216167972bb2a0d4a9ac533ef578d25
Zamir Kanji <zamirkanji@comcast.net>
2022-01-03 10:32:04 -0800

    setup webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const pageLoad = () => {
+    document.addEventListener('DOMContentLoaded', function() {
+        // alert("Ready!");
+        
+    }, false)
+}
+
+export default pageLoad;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import pageLoad from './app.js';
+import './style.css';
+
+// pageLoad();
+
+
+const mainDiv = document.querySelector('#content');
+mainDiv.textContent = 'Hello';
+mainDiv.classList.add('main-container');
+
+
+console.log('test');
